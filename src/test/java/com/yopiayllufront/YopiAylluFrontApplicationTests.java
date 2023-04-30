5fc364ad68d9c7cf27f48b23d765040e6993d6d5
YopiCode <vallns643@gmail.com>
2023-04-30 00:39:36 -0500

    Comienzo :D

---
+++ src/test/java/com/yopiayllufront/YopiAylluFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yopiayllufront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YopiAylluFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
