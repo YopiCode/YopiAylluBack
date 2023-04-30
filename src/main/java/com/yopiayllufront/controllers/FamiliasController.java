39f8f43005052e8583a0f557cbf6bb2531e2dbc7
Theleo <theleo1707@gmail.com>
2023-04-30 12:35:18 -0500

    Modelos Terminados 2 :D

--- src/main/java/com/yopiayllufront/controllers/FamiliasController.java
+++ src/main/java/com/yopiayllufront/controllers/FamiliasController.java
@@ -1,14 +1,21 @@
 package com.yopiayllufront.controllers;
 
+import com.yopiayllufront.models.Familias;
 import com.yopiayllufront.models.Integrantes;
+import com.yopiayllufront.repositories.FamiliaRepository;
 import com.yopiayllufront.repositories.IntegranteRepository;
 import com.yopiayllufront.services.IntegrantesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 public class FamiliasController {
@@ -16,9 +23,17 @@ public class FamiliasController {
     @Autowired
     IntegrantesService integrantesService;
 
-    @GetMapping("/familiares")
-    public ArrayList<Integrantes> getfamiliares(@Param("codigo") int codigo){
-        return integrantesService.getAllIntegrantesByCodigo(codigo);
+    @Autowired
+    FamiliaRepository familiaRepository;
+
+//    @GetMapping("/familiares")
+//    public ArrayList<Integrantes> getfamiliares(@Param("codigo") int codigo){
+//        return integrantesService.getAllIntegrantesByCodigo(codigo);
+//    }
+
+    @PostMapping("/familiares/registro")
+    public Familias registrar_familiar(@RequestBody Familias familias){
+        return familiaRepository.save(familias);
     }
 
 
