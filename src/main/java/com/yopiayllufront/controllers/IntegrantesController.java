9b6fb93401333b8bdbb1e0d4a9963809e9081585
Theleo <theleo1707@gmail.com>
2023-04-30 19:39:46 -0500

    Login, registro de nuevo usuario e ingreso de un nuevo familiar

---
+++ src/main/java/com/yopiayllufront/controllers/IntegrantesController.java
@@ -0,0 +1,36 @@
+package com.yopiayllufront.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yopiayllufront.models.Familias;
+import com.yopiayllufront.models.Integrantes;
+import com.yopiayllufront.repositories.FamiliasRepository;
+import com.yopiayllufront.repositories.IntegrantesRepository;
+import com.yopiayllufront.services.IntegrantesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+public class IntegrantesController {
+
+    @Autowired
+    IntegrantesRepository integrantesRepository;
+
+    @Autowired
+    IntegrantesService integrantesService;
+
+    @PostMapping("familia")
+    public Integrantes registrar_integrante(@RequestBody HashMap<String, Object> integrante){
+        ObjectMapper objectMapper = new ObjectMapper();
+        Integrantes integranteObj = objectMapper.convertValue(integrante.get("integrante"), Integrantes.class);
+        int cod_famili = (int)integrante.get("codigofamiliar");
+        return integrantesRepository.save(integrantesService.integrantes_familia(cod_famili,integranteObj));
+    }
+
+
+
+
+}
